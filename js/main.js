b8287d1fa634dcac31cc0503d595f7c9bce73d76
aidanakmatova13 <akmatova.don@gmail.com>
2021-06-25 23:03:57 +0300

    fix burger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+const mobileMenu = document.querySelector('.mobile-menu')
+const topMenu = document.querySelector('.top-menu')
+const header = document.querySelector('.header')
+
+mobileMenu.addEventListener('click', () => {
+    topMenu.classList.toggle('show-menu')
+    header.classList.toggle('header-active')
+})
+
+const menuLinks = document.querySelectorAll('.top-menu li a')
+
+menuLinks.forEach(element => element.addEventListener('click', () => {
+    topMenu.classList.remove('show-menu')
+}))
+
 $(document).ready(function($) {
     $('.count-number').counterUp({
         delay: 5,
         time: 3500
     });
 });
+
+
